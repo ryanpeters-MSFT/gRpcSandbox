6dc701b73c795aa21386d911e7927cd7eb4efd5d
Ryan Peters <ryanpeters@microsoft.com>
2024-05-06 11:59:32 -0400

    remove unused logger

--- CodeFirst/Service/Services/GreeterService.cs
+++ CodeFirst/Service/Services/GreeterService.cs
@@ -5,13 +5,6 @@ namespace Service.Services
 {
     public class GreeterService : IGreeterService
     {
-        private readonly ILogger<GreeterService> _logger;
-
-        public GreeterService(ILogger<GreeterService> logger)
-        {
-            _logger = logger;
-        }
-
         public Task<HelloReply> SayHello(HelloRequest request, CallContext context = default)
         {
             var names = request.Name.Split(" ");
